4860a1e12c4acee8b4a85015fe01cf795eef4110
Eria <c121680118@gmail.com>
2019-12-01 15:10:24 +0800

    first commit

---
+++ student-4-2/13 QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,10 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        if(question == "Hi")
+        {
+            return "Hi"
+        }
+        // TODO: Write a response
+        return "?"
+    }
+}
